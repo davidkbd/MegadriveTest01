45ad2615dee0bad8e967a90026c3f443225ac2e7
davidkbd <davidkbd@davidkbd-Vostro-3558>
2018-11-07 01:06:04 +0100

    empiezo a a cambiar fondos a deathegg

--- res/gfx.res
+++ res/gfx.res
@@ -7,18 +7,16 @@ IMAGE portada_opts_palete "tiles/portada_menu.png" BEST
 
 IMAGE pausa_screen "gfx/pausascreen.png" BEST
 
-IMAGE planb_level01 "gfx/ingame_planb.png" BEST
-
 TILESET hud_nums_tile "tiles/hud_nums.png" NONE
 
-TILESET ingame_hud_tile "gfx/ingame_hud.png" NONE
-TILESET ingame_floor_tile "gfx/ingame_floor.png" NONE
-TILESET ingame_floor2_tile "gfx/ingame_floor2.png" NONE
-TILESET ingame_background_tile "gfx/ingame_background.png" NONE
-TILESET ingame_background2_tile "gfx/ingame_background2.png" NONE
-TILESET ingame_background3_tile "gfx/ingame_background3.png" NONE
-TILESET ingame_background4_tile "gfx/ingame_background4.png" NONE
-TILESET ingame_background5_tile "gfx/ingame_background5.png" NONE
-TILESET ingame_background6_tile "gfx/ingame_background6.png" NONE
-TILESET ingame_wall_tile "gfx/ingame_wall.png" NONE
+IMAGE planb_level01 "gfx/deathegg_taller/ingame_planb.png" BEST
+TILESET ingame_floor_tile "gfx/deathegg_taller/ingame_floor.png" NONE
+TILESET ingame_floor2_tile "gfx/deathegg_taller/ingame_floor2.png" NONE
+TILESET ingame_background_tile "gfx/deathegg_taller/ingame_background.png" NONE
+TILESET ingame_background2_tile "gfx/deathegg_taller/ingame_background2.png" NONE
+TILESET ingame_background3_tile "gfx/deathegg_taller/ingame_background3.png" NONE
+TILESET ingame_background4_tile "gfx/deathegg_taller/ingame_background4.png" NONE
+TILESET ingame_background5_tile "gfx/deathegg_taller/ingame_background5.png" NONE
+TILESET ingame_background6_tile "gfx/deathegg_taller/ingame_background6.png" NONE
+TILESET ingame_wall_tile "gfx/deathegg_taller/ingame_wall.png" NONE
 
--- res/gfx/togenerador.sh
+++ res/gfx/togenerador.sh
@@ -1,14 +1,14 @@
 for i in $(seq 1 16)
 do
 	size=$[ ${i} * 16 - 16 ]
-	convert ingame_floor.png       -crop 16x16+0+${size} ../../map_gen/tiles/${i}.png
-	convert ingame_floor2.png      -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 16].png
-	convert ingame_wall.png        -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 32].png
-	convert ingame_background.png  -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 48].png
-	convert ingame_background2.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 64].png
-	convert ingame_background3.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 80].png
-	convert ingame_background4.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 96].png
-	convert ingame_background5.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 112].png
-	convert ingame_background6.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 128].png
+	convert deathegg_taller/ingame_floor.png       -crop 16x16+0+${size} ../../map_gen/tiles/${i}.png
+	convert deathegg_taller/ingame_floor2.png      -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 16].png
+	convert deathegg_taller/ingame_wall.png        -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 32].png
+	convert deathegg_taller/ingame_background.png  -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 48].png
+	convert deathegg_taller/ingame_background2.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 64].png
+	convert deathegg_taller/ingame_background3.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 80].png
+	convert deathegg_taller/ingame_background4.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 96].png
+	convert deathegg_taller/ingame_background5.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 112].png
+	convert deathegg_taller/ingame_background6.png -crop 16x16+0+${size} ../../map_gen/tiles/$[${i} + 128].png
 done
 
